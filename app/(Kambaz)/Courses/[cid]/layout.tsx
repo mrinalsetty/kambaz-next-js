9247a26964c0125b43cc8d7e270d1102a2e084ee
Mrinal Setty <mrinalsetty.s@gmail.com>
2025-09-15 01:22:23 -0400

    Resolve Vercel Deployment issue

--- app/(Kambaz)/Courses/[cid]/layout.tsx
+++ app/(Kambaz)/Courses/[cid]/layout.tsx
@@ -3,8 +3,8 @@ import CourseNavigation from "./Navigation";
 export default async function CoursesLayout({
   children,
   params,
-}: Readonly<{ children: ReactNode; params: Promise<{ id: string }> }>) {
-  const { id: cid } = await params;
+}: Readonly<{ children: ReactNode; params: Promise<{ cid: string }> }>) {
+  const { cid } = await params;
   return (
     <div id="wd-courses">
       <h2>Courses {cid}</h2>
