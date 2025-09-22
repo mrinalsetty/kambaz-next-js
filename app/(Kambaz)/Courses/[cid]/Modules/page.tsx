16258d5044a14aa86a18fe4411e59503a725b51c
Mrinal Setty <mrinalsetty.s@gmail.com>
2025-09-22 15:40:29 -0400

    Update Modules/page.tsx

--- app/(Kambaz)/Courses/[cid]/Modules/page.tsx
+++ app/(Kambaz)/Courses/[cid]/Modules/page.tsx
@@ -3,8 +3,8 @@ export default function Modules() {
     <div>
       <button id="wd-modules-collapse-all">Collapse All</button>{" "}
       <button id="wd-modules-view-progress">View Progress</button>{" "}
-      <select id="wd-modules-select">
-        <option defaultValue="Publish All">Publish All</option>
+      <select id="wd-modules-select" defaultValue="Publish All">
+        <option value="Publish All">Publish All</option>
       </select>{" "}
       <button id="wd-modules-add">+ Module</button>
       <ul id="wd-modules">
@@ -54,11 +54,41 @@ export default function Modules() {
           <div className="wd-title">
             Week 1, Lecture 2 - Formatting User Interfaces with HTML
           </div>
+          <ul className="wd-lessons">
+            <li className="wd-lesson">
+              <span className="wd-title">LEARNING OBJECTIVES</span>
+              <ul className="wd-content">
+                <li className="wd-content-item">
+                  Learn how to create user interfaces with HTML
+                </li>
+                <li className="wd-content-item">
+                  Deploy the assignment to Netlify
+                </li>
+              </ul>
+            </li>
+            <li className="wd-lesson">
+              <span className="wd-title">SLIDES</span>
+              <ul className="wd-content">
+                <li className="wd-content-item">
+                  Introduction to HTML and the DOM
+                </li>
+                <li className="wd-content-item">
+                  Creating an HTTP server with Node.js
+                </li>
+                <li className="wd-content-item">
+                  Formatting Web content with Headings and
+                </li>
+                <li className="wd-content-item">
+                  Formatting content with Lists and Tables
+                </li>
+              </ul>
+            </li>
+          </ul>
         </li>
         <li className="wd-module">
           <div className="wd-title">Week 2</div>
         </li>
-        <li>
+        <li className="wd-module">
           <div className="wd-title">Week 3</div>
         </li>
       </ul>
