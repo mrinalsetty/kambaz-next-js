0905d7e13dd5415befca1cf00b6c239422df9531
Mrinal Setty <mrinalsetty.s@gmail.com>
2025-10-20 18:56:05 -0400

    update lab 3

--- app/(Kambaz)/Account/Navigation.tsx
+++ app/(Kambaz)/Account/Navigation.tsx
@@ -1,33 +1,36 @@
+"use client";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 export default function AccountNavigation() {
-  const spacer = <div style={{ height: "0.5rem" }} />;
+  const pathname = usePathname();
+  const links = [
+    { label: "Signin", href: "/Account/Signin", id: "wd-account-signin-link" },
+    { label: "Signup", href: "/Account/Signup", id: "wd-account-signup-link" },
+    {
+      label: "Profile",
+      href: "/Account/Profile",
+      id: "wd-account-profile-link",
+    },
+  ];
 
   return (
     <div id="wd-account-navigation" className="wd list-group fs-5 rounded-0">
-      <Link
-        href="/Account/Signin"
-        id="wd-account-signin-link"
-        className="list-group-item active border-0 py-2"
-      >
-        Signin
-      </Link>
-      {spacer}
-      <Link
-        href="/Account/Signup"
-        id="wd-account-signup-link"
-        className="list-group-item text-danger border-0 py-2"
-      >
-        Signup
-      </Link>
-      {spacer}
-      <Link
-        href="/Account/Profile"
-        id="wd-account-profile-link"
-        className="list-group-item text-danger border-0 py-2"
-      >
-        Profile
-      </Link>
+      {links.map(({ label, href, id }) => {
+        const active = pathname.startsWith(href);
+        return (
+          <Link
+            key={href}
+            href={href}
+            id={id}
+            className={`list-group-item border-0 py-2 ${
+              active ? "active" : "text-danger"
+            }`}
+          >
+            {label}
+          </Link>
+        );
+      })}
     </div>
   );
 }
