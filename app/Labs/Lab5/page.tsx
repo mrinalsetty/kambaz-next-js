9eba378cfb202e2a91ea141c2a793a8c6da6d268
Mrinal Setty <mrinalsetty.s@gmail.com>
2025-11-17 13:43:41 -0500

    a5

--- app/Labs/Lab5/page.tsx
+++ app/Labs/Lab5/page.tsx
@@ -1,10 +1,16 @@
+import EnvironmentVariables from "./EnvironmentVariables";
+import PathParameters from "./PathParameters";
+import QueryParameters from "./QueryParameters";
+import WorkingWithArrays from "./WorkingWithArrays";
+import WorkingWithObjects from "./WorkingWithObjects";
+const HTTP_SERVER = process.env.NEXT_PUBLIC_HTTP_SERVER;
 export default function Lab5() {
   return (
     <div id="wd-lab5">
       <h2>Lab 5</h2>
       <div className="list-group">
         <a
-          href="http://localhost:4000/lab5/welcome"
+          href={`${HTTP_SERVER}/lab5/welcome`}
           className="list-group-item"
           id="wd-lab5-welcome-link"
         >
@@ -12,6 +18,11 @@ export default function Lab5() {
         </a>
       </div>
       <hr />
+      <EnvironmentVariables />
+      <PathParameters />
+      <QueryParameters />
+      <WorkingWithObjects />
+      <WorkingWithArrays />
     </div>
   );
 }
